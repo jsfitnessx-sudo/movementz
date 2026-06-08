a9ee498ca8dff86a26702aed653280ccbe0a3851
Joseph <jsfitnessx@gmail.com>
2026-06-08 18:55:58 +1000

    Remove duplicate header logo icon

--- src/components/brand/BrandMark.jsx
+++ src/components/brand/BrandMark.jsx
@@ -1,9 +1,8 @@
-import { movementzIconSrc, movementzWordmarkSrc } from "../../lib/brandAssets.js";
+import { movementzWordmarkSrc } from "../../lib/brandAssets.js";
 
 export function BrandMark() {
   return (
     <div className="brand-mark" aria-label="Movementz">
-      <img className="brand-icon-img" src={movementzIconSrc} alt="" />
       <img className="brand-wordmark-img" src={movementzWordmarkSrc} alt="Movementz" />
     </div>
   );
